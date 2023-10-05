834a5960e2cba3d261e01684cb5084001e3e0919
sotatek-tyler-nguyen <tyler.nguyen3@sotatek.com>
2023-10-05 16:25:08 +0700

    [ECO-552] - Fix bash file

--- src/frontend/vercel-submodule-workaround.sh
+++ src/frontend/vercel-submodule-workaround.sh
@@ -27,12 +27,13 @@ cd tmp # go into the tmp folder
 # checkout the current submodule commit
 git init # initialise empty repo
 git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
-# git fetch --depth=1 origin $COMMIT # fetch only the required version
-# git checkout $COMMIT # checkout on the right commit
+git fetch origin master # fetch only the required version
+git checkout master # checkout on the right commit
 
 # move the submodule from tmp to the submodule path
 cd .. # go folder up
 rm -rf tmp/.git # remove .git 
+ls
 mv tmp/* $SUBMODULE_PATH/ # move the submodule to the submodule path
 
 # clean up
