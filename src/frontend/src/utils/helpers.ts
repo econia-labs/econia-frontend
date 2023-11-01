34581179b5c746fa5e699649a6f9c4a549c05400
sotatek-tyler-nguyen <tyler.nguyen3@sotatek.com>
2023-11-01 16:32:20 +0700

    [ECO-831] - Config chart trading view

--- src/frontend/src/utils/helpers.ts
+++ src/frontend/src/utils/helpers.ts
@@ -20,35 +20,35 @@ export async function makeApiRequest(path: string) {
 
 export async function getAllMarket(currency: string = "usd") {
   return MOCK_MARKETS;
-  const res = await makeApiRequest(
-    `api/v3/coins/markets?vs_currency=${currency}`,
-  );
-  const allMarketData =
-    res && res.length > 0
-      ? (res as MarketRes[]).map(
-          (item) =>
-            ({
-              ...item,
-              market_id: item.id,
-              name: `${item.symbol}-usd`.toUpperCase(),
-              base: {
-                name: item.name,
-                symbol: item.symbol,
-                decimals: 8,
-              },
-              quote: {
-                name: "usd",
-                symbol: "usd",
-                decimals: 6,
-              },
-              lot_size: 1,
-              tick_size: 1,
-              min_size: 1,
-              underwriter_id: 0,
-            } as MarketData),
-        )
-      : [];
-  return allMarketData;
+  // const res = await makeApiRequest(
+  //   `api/v3/coins/markets?vs_currency=${currency}`,
+  // );
+  // const allMarketData =
+  //   res && res.length > 0
+  //     ? (res as MarketRes[]).map(
+  //         (item) =>
+  //           ({
+  //             ...item,
+  //             market_id: item.id,
+  //             name: `${item.symbol}-usd`.toUpperCase(),
+  //             base: {
+  //               name: item.name,
+  //               symbol: item.symbol,
+  //               decimals: 8,
+  //             },
+  //             quote: {
+  //               name: "usd",
+  //               symbol: "usd",
+  //               decimals: 6,
+  //             },
+  //             lot_size: 1,
+  //             tick_size: 1,
+  //             min_size: 1,
+  //             underwriter_id: 0,
+  //           } as MarketData),
+  //       )
+  //     : [];
+  // return allMarketData;
 }
 
 export async function makeApiRequestMin(path: string) {
