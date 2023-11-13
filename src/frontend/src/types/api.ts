36762720b5fd9a842040fc35750c1be1626f9b43
sotatek-stephen-pham <stephen.pham@sotatek.com>
2023-11-13 15:40:13 +0700

    Update orders table UI, nominal price

--- src/frontend/src/types/api.ts
+++ src/frontend/src/types/api.ts
@@ -76,7 +76,7 @@ export type ApiOrder = {
   integrator: string;
   total_filled: number;
   remaining_size: number;
-  average_execution_price: number;
+  average_execution_price: number | null;
   order_status: string;
   order_type: string;
   price?: number;
