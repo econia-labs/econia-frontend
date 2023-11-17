26778fa814b2a17d7904ac03c42e2675e8d1eb20
sotatek-stephen-pham <145954854+sotatek-stephen-pham@users.noreply.github.com>
2023-11-17 14:32:32 +0700

    [ECO-928] - Update trade history table (#52)

--- src/frontend/src/pages/market/[market_id].tsx
+++ src/frontend/src/pages/market/[market_id].tsx
@@ -375,7 +375,7 @@ export default function Market({ allMarketData, marketData }: Props) {
             </div>
             <div className="scrollbar-none mt-3 h-full max-h-full min-h-[160px] overflow-auto border border-neutral-600">
               <p className="sticky top-0 bg-neutral-800 bg-noise py-3 pl-4 font-jost font-bold text-white">
-                Trade History
+                Market Trade History
               </p>
               <TradeHistoryTable
                 marketData={marketData}
